da25a8e207d3ace72fea08e5c306e943f1b9b896
glacreative <glacreative@gmail.com>
2021-06-05 10:16:12 +0300

    Add Song Comments
    For Pirple Ass

--- song.kt
+++ song.kt
@@ -1,3 +1,34 @@
-fun fun main(args: Array<String>) {
-    print ('test')
+fun main(args: Array<String>) {
+    //Song Name in the you tube
+    var song = "Heena Maka"
+    //Album Name
+    var album = "Dura Akahe"
+    //Released Year
+    var year = "2020"
+    /* People who sing
+    the song */
+    var artist = "Harshadewa & RaviJay"
+    //Which Category
+    var genres = "Pop"
+    //Song Duration
+    var duration = "4.15"
+    //Song Writers
+    var writer = "Arosha Adhikari, Ravi Jay & Charitha Attalage"
+    //Person who done the music composing
+    var composer = "Charitha Attalage"
+    //Which mood
+    var mood = "Love"
+
+    println(song)
+    println(album)
+    println(year)
+    println(artist)
+    println(genres)
+    println(duration)
+    println(writer)
+    println(composer)
+    println(mood)
 }
+
+
+
